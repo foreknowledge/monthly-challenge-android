4ee2f05eea81b8abcc6d6796200cf5a1fd67980f
foreknowledge <kyj_0129@naver.com>
2021-07-11 04:03:05 +0900

    feat: 챌린지 조회 액티비티 구현

--- app/src/main/java/com/gdghackathon/monthlychallenges/ui/ChallengeContentsActivity.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/ui/ChallengeContentsActivity.kt
@@ -33,8 +33,6 @@ class ChallengeContentsActivity : AppCompatActivity() {
 
     private fun subscribeUI() {
         challengeViewModel.challenge.observe(this, {
-            binding.challenge = it
-
             val missionList = it.missionList?.toMutableList() ?: mutableListOf()
             binding.missionList.adapter = MissionListRecyclerAdapter(missionList, editable = false).apply {
                 setOnItemClickListener {
