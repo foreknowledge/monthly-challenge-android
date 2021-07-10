f3ef1a5d8f176575b34c18bdb57996372fc6be99
foreknowledge <kyj_0129@naver.com>
2021-07-10 13:52:33 +0900

    feat: ViewModel 사용 샘플 추가

--- app/src/main/java/com/gdghackathon/monthlychallenges/main/MainActivity.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/main/MainActivity.kt
@@ -2,11 +2,23 @@ package com.gdghackathon.monthlychallenges.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.gdghackathon.monthlychallenges.R
+import com.gdghackathon.monthlychallenges.viewmodel.ChallengeListViewModel
 
 class MainActivity : AppCompatActivity() {
+    private val challengeListViewModel by lazy {
+        ViewModelProvider(this).get(ChallengeListViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val challengeList = challengeListViewModel.challengeList.value ?: listOf()
+        challengeList.forEach {
+            Log.d("test", "${it.id}, ${it.name}, ${it.missionCount}")
+        }
     }
 }
