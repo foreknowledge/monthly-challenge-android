b0e217b153f765cdeed0d4ac791e294e15ebf6ca
foreknowledge <kyj_0129@naver.com>
2021-07-10 12:58:01 +0900

    feat: Coroutine 적용 선행작업 - 네트워크 통신 함수 suspend function으로 변경

--- app/src/main/java/com/gdghackathon/monthlychallenges/model/ChallengeRepository.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/model/ChallengeRepository.kt
@@ -6,26 +6,26 @@ object ChallengeRepository {
     private val challengeDataSource = ChallengeDataSource()
 
     // 챌린지 생성
-    fun createChallenge(name: String, missionList: List<Mission>) =
+    suspend fun createChallenge(name: String, missionList: List<Mission>) =
         challengeDataSource.createChallenge(name, missionList)
 
     // 챌린지 삭제
-    fun deleteChallenge(challengeId: Long) =
+    suspend fun deleteChallenge(challengeId: Long) =
         challengeDataSource.deleteChallenge(challengeId)
 
     // 특정 챌린지 조회
-    fun getChallenge(challengeId: Long) =
+    suspend fun getChallenge(challengeId: Long) =
         challengeDataSource.getChallenge(challengeId)
 
     // 챌린지 샘플 조회
-    fun getAllSampleChallenges() =
+    suspend fun getAllSampleChallenges() =
         challengeDataSource.getAllSampleChallenges()
 
     // 챌린지 미션 조회
-    fun getAllMissions(challengeId: Long) =
+    suspend fun getAllMissions(challengeId: Long) =
         challengeDataSource.getAllMissions(challengeId)
 
     // 챌린지 미션 인증(사진 업로드)
-    fun completeMission(challengeId: Long, missionId: Long, image: Bitmap, memo: String?) =
+    suspend fun completeMission(challengeId: Long, missionId: Long, image: Bitmap, memo: String?) =
         challengeDataSource.completeMission(challengeId, missionId, image, memo)
 }
--- app/src/main/java/com/gdghackathon/monthlychallenges/model/RetrofitService.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/model/RetrofitService.kt
@@ -8,37 +8,37 @@ interface RetrofitService {
     // 챌린지 생성
     @FormUrlEncoded
     @POST("challenge")
-    fun createChallenge(
+    suspend fun createChallenge(
         @Field("name") name: String,
         @Field("mission") missionList: List<Mission>
     ): Call<Long>
 
     // 챌린지 삭제
     @DELETE("challenge/{challengeId}")
-    fun deleteChallenge(
+    suspend fun deleteChallenge(
         @Path("challengeId") challengeId: Long
     ): Call<Boolean>
 
     // 특정 챌린지 조회 (미션 포함)
     @GET("challenge/{challengeId}")
-    fun getChallenge(
+    suspend fun getChallenge(
         @Path("challengeId") challengeId: Long
     ): Call<Challenge>
 
     // 챌린지 샘플 데이터 조회 (8개, 미션 포함)
     @GET("challenge/samples")
-    fun getAllSampleChallenges(): Call<List<Challenge>>
+    suspend fun getAllSampleChallenges(): Call<List<Challenge>>
 
     // 챌린지 미션 조회 (전체 미션)
     @GET("challenge/{challengeId}/mission")
-    fun getAllMissions(
+    suspend fun getAllMissions(
         @Path("challengeId") challengeId: Long
     ): Call<List<Mission>>
 
     // 챌린지 미션 인증 (챌린지 미션에 사진 올리기)
     @Multipart
     @POST("challenge/{challengeId}/mission/{missionId}")
-    fun completeMission(
+    suspend fun completeMission(
         @Path("challengeId") challengeId: Long,
         @Path("missionId") missionId: Long,
         @Part("image") image: Bitmap,
