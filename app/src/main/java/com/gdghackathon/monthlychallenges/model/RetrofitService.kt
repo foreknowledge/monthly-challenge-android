5fcc0bb4371e9bd96d7bca37266ba8b19a3660c0
foreknowledge <kyj_0129@naver.com>
2021-07-11 02:11:17 +0900

    feat: RetrofitService 인터페이스 반환 값 Call 제거

--- app/src/main/java/com/gdghackathon/monthlychallenges/model/RetrofitService.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/model/RetrofitService.kt
@@ -1,7 +1,6 @@
 package com.gdghackathon.monthlychallenges.model
 
 import android.graphics.Bitmap
-import retrofit2.Call
 import retrofit2.http.*
 
 interface RetrofitService {
@@ -11,29 +10,29 @@ interface RetrofitService {
     suspend fun createChallenge(
         @Field("name") name: String,
         @Field("mission") missionList: List<Mission>
-    ): Call<Long>
+    ): Long
 
     // 챌린지 삭제
     @DELETE("challenge/{challengeId}")
     suspend fun deleteChallenge(
         @Path("challengeId") challengeId: Long
-    ): Call<Boolean>
+    ): Boolean
 
     // 특정 챌린지 조회 (미션 포함)
     @GET("challenge/{challengeId}")
     suspend fun getChallenge(
         @Path("challengeId") challengeId: Long
-    ): Call<Challenge>
+    ): Challenge
 
     // 챌린지 샘플 데이터 조회 (8개, 미션 포함)
     @GET("challenge/samples")
-    suspend fun getAllSampleChallenges(): Call<List<Challenge>>
+    suspend fun getAllSampleChallenges(): List<Challenge>
 
     // 챌린지 미션 조회 (전체 미션)
     @GET("challenge/{challengeId}/mission")
     suspend fun getAllMissions(
         @Path("challengeId") challengeId: Long
-    ): Call<List<Mission>>
+    ): List<Mission>
 
     // 챌린지 미션 인증 (챌린지 미션에 사진 올리기)
     @Multipart
@@ -43,5 +42,5 @@ interface RetrofitService {
         @Path("missionId") missionId: Long,
         @Part("image") image: Bitmap,
         @Part("memo") memo: String?
-    ): Call<Mission>
+    ): Mission
 }
