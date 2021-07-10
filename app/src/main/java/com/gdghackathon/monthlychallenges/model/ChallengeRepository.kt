e9d44f73dbb574f97426d2eeda138cf5fd7f3885
Yang Cheol Joo <rlaengks0001@naver.com>
2021-07-10 09:07:53 +0900

    refactor: 표현식 본문 사용

--- app/src/main/java/com/gdghackathon/monthlychallenges/model/ChallengeRepository.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/model/ChallengeRepository.kt
@@ -6,32 +6,26 @@ class ChallengeRepository {
     private val challengeDataSource = ChallengeDataSource()
 
     // 챌린지 생성
-    fun createChallenge(name: String, tag: List<String>): Boolean {
-        return challengeDataSource.createChallenge(name, tag)
-    }
+    fun createChallenge(name: String, tag: List<String>)
+            = challengeDataSource.createChallenge(name, tag)
 
     // 챌린지 삭제
-    fun deleteChallenge(challengeId: Long): Boolean {
-        return challengeDataSource.deleteChallenge(challengeId)
-    }
+    fun deleteChallenge(challengeId: Long)
+            = challengeDataSource.deleteChallenge(challengeId)
 
     // 챌린지 조회(챌린지 목록)
-    fun getAllChallenges(): List<Challenge> {
-        return challengeDataSource.getAllChallenges()
-    }
+    fun getAllChallenges()
+            = challengeDataSource.getAllChallenges()
 
     // 챌린지 샘플 조회
-    fun getAllSampleChallenges(): List<Challenge> {
-        return challengeDataSource.getAllSampleChallenges()
-    }
+    fun getAllSampleChallenges()
+            = challengeDataSource.getAllSampleChallenges()
 
     // 챌린지 미션 조회
-    fun getAllMissions(challengeId: Long): List<Mission> {
-        return challengeDataSource.getAllMissions(challengeId)
-    }
+    fun getAllMissions(challengeId: Long)
+            = challengeDataSource.getAllMissions(challengeId)
 
     // 챌린지 미션 인증(사진 업로드)
-    fun completeMission(challengeId: Long, missionId: Long, image: Bitmap, memo: String?): Boolean {
-        return challengeDataSource.completeMission(challengeId, missionId, image, memo)
-    }
+    fun completeMission(challengeId: Long, missionId: Long, image: Bitmap, memo: String?)
+            = challengeDataSource.completeMission(challengeId, missionId, image, memo)
 }
