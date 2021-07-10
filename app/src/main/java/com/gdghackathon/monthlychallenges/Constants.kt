56d99e5330ff4184763c294cce5fd348bf7a564c
foreknowledge <kyj_0129@naver.com>
2021-07-11 04:18:41 +0900

    feat: 완료 미션 개수에 따라 챌린이 이미지 바뀌도록 설정

--- app/src/main/java/com/gdghackathon/monthlychallenges/Constants.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/Constants.kt
@@ -1,3 +1,5 @@
 package com.gdghackathon.monthlychallenges
 
 const val NUM_OF_MISSIONS = 30
+const val BOUNDARY_CHALLINEY_1 = 10
+const val BOUNDARY_CHALLINEY_2 = 20
--- app/src/main/java/com/gdghackathon/monthlychallenges/utils/BindingAdapters.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/utils/BindingAdapters.kt
@@ -1,8 +1,13 @@
 package com.gdghackathon.monthlychallenges.utils
 
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
+import com.gdghackathon.monthlychallenges.BOUNDARY_CHALLINEY_1
+import com.gdghackathon.monthlychallenges.BOUNDARY_CHALLINEY_2
 import com.gdghackathon.monthlychallenges.NUM_OF_MISSIONS
+import com.gdghackathon.monthlychallenges.R
 
 @BindingAdapter("set_startDate")
 fun TextView.setStartDate(date: String?) {
@@ -18,5 +23,16 @@ fun TextView.setMissionCount(count: Int?) {
     count?.let {
         val missionCount = "$count/$NUM_OF_MISSIONS"
         text = missionCount
+    }
+}
+
+@BindingAdapter("set_image")
+fun ImageView.setImage(count: Int?) {
+    count?.let {
+        val resId = if (count <= BOUNDARY_CHALLINEY_1) R.drawable.challiney_1
+        else if (count <= BOUNDARY_CHALLINEY_2) R.drawable.challiney_2
+        else R.drawable.challiney_3
+
+        setImageDrawable(ResourcesCompat.getDrawable(context.resources, resId, null))
     }
 }
