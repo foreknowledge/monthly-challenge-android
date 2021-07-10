9f8119b690fdaed563de319160b2d438570eaa4c
foreknowledge <kyj_0129@naver.com>
2021-07-10 20:47:43 +0900

    feat: 리스트 화면 구현 작업 - 테스트용 토스트 유틸리티 추가

---
+++ app/src/main/java/com/gdghackathon/monthlychallenges/utils/ToastUtil.kt
@@ -0,0 +1,9 @@
+package com.gdghackathon.monthlychallenges.utils
+
+import android.content.Context
+import android.widget.Toast
+
+object ToastUtil {
+    fun showToast(context: Context, msg: String) =
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+}
