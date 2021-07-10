7c1de942f4a510681eb827a154ac49da6bdf7158
foreknowledge <kyj_0129@naver.com>
2021-07-11 01:31:58 +0900

    feat: MainActivity 데이터바인딩 사용해서 데이터 보여주도록 변경 (Mock Data 사용)

--- app/src/main/java/com/gdghackathon/monthlychallenges/ui/MainActivity.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/ui/MainActivity.kt
@@ -3,12 +3,11 @@ package com.gdghackathon.monthlychallenges.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.gdghackathon.monthlychallenges.R
+import com.gdghackathon.monthlychallenges.databinding.ActivityMainBinding
 import com.gdghackathon.monthlychallenges.model.Challenge
 import com.gdghackathon.monthlychallenges.viewmodel.SampleChallengesViewModel
 
@@ -17,41 +16,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         ViewModelProvider(this).get(SampleChallengesViewModel::class.java)
     }
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val createChallengeButton = findViewById<Button>(R.id.button_create_challenge)
-        val sample1Button = findViewById<Button>(R.id.button_1)
-        val sample2Button = findViewById<Button>(R.id.button_2)
-        val sample3Button = findViewById<Button>(R.id.button_3)
-        val sample4Button = findViewById<Button>(R.id.button_4)
-        val sample5Button = findViewById<Button>(R.id.button_5)
-        val sample6Button = findViewById<Button>(R.id.button_6)
-        val sample7Button = findViewById<Button>(R.id.button_7)
-        val sample8Button = findViewById<Button>(R.id.button_8)
-        val completeChallengeButton = findViewById<ImageButton>(R.id.imagebutton_complete_challenge_list)
+        binding.buttonCreateChallenge.setOnClickListener(this)
+        binding.button1.setOnClickListener(this)
 
-        createChallengeButton.setOnClickListener(this)
-        sample1Button.setOnClickListener(this)
-        sample2Button.setOnClickListener(this)
-        sample3Button.setOnClickListener(this)
-        sample4Button.setOnClickListener(this)
-        sample5Button.setOnClickListener(this)
-        sample6Button.setOnClickListener(this)
-        sample7Button.setOnClickListener(this)
-        sample8Button.setOnClickListener(this)
-        completeChallengeButton.setOnClickListener(this)
-
-        val sampleChallenges = sampleChallengesViewModel.sampleChallenges.value
-            ?: listOf(
-                Challenge(1, "소확도", 0),
-                Challenge(2, "극뽁", 0),
-                Challenge(3, "운동", 0),
-            )
-        sampleChallenges.forEach {
-            Log.d("test", "${it.id}, ${it.name}, ${it.missionCount}")
-        }
+        sampleChallengesViewModel.loadData(getAllSampleChallenges())
+        sampleChallengesViewModel.sampleChallenges.observe(this, {
+            binding.sampleList = it
+        })
     }
 
     override fun onClick(v: View?) {
@@ -61,15 +38,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(intent)
             }
             R.id.button_1 -> { }
-            R.id.button_2 -> { }
-            R.id.button_3 -> { }
-            R.id.button_4 -> { }
-            R.id.button_5 -> { }
-            R.id.button_6 -> { }
-            R.id.button_7 -> { }
-            R.id.button_8 -> { }
-            R.id.imagebutton_complete_challenge_list -> { }
             else -> { }
         }
     }
+
+    private fun getAllSampleChallenges() = listOf(
+            Challenge(0, resources.getString(R.string.main_button1_text), 0),
+            Challenge(1, resources.getString(R.string.main_button2_text), 0),
+            Challenge(2, resources.getString(R.string.main_button3_text), 0),
+            Challenge(3, resources.getString(R.string.main_button4_text), 0),
+            Challenge(4, resources.getString(R.string.main_button5_text), 0),
+            Challenge(5, resources.getString(R.string.main_button6_text), 0),
+            Challenge(6, resources.getString(R.string.main_button7_text), 0),
+            Challenge(7, resources.getString(R.string.main_button8_text), 0)
+    )
 }
