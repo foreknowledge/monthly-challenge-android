63836bfd281c61ff92185314bb6d489820a7acb2
foreknowledge <kyj_0129@naver.com>
2021-07-10 14:50:49 +0900

    refactor: 클래스 및 함수명 변경

--- app/src/main/java/com/gdghackathon/monthlychallenges/main/MainActivity.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/main/MainActivity.kt
@@ -6,18 +6,18 @@ import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.gdghackathon.monthlychallenges.R
 import com.gdghackathon.monthlychallenges.model.Challenge
-import com.gdghackathon.monthlychallenges.viewmodel.ChallengeListViewModel
+import com.gdghackathon.monthlychallenges.viewmodel.SampleChallengesViewModel
 
 class MainActivity : AppCompatActivity() {
     private val challengeListViewModel by lazy {
-        ViewModelProvider(this).get(ChallengeListViewModel::class.java)
+        ViewModelProvider(this).get(SampleChallengesViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val challengeList = challengeListViewModel.challengeList.value
+        val challengeList = challengeListViewModel.sampleChallenges.value
             ?: listOf(
                 Challenge(1, "소확도", 0),
                 Challenge(2, "극뽁", 0),
--- app/src/main/java/com/gdghackathon/monthlychallenges/viewmodel/SampleChallengesViewModel.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/viewmodel/SampleChallengesViewModel.kt
@@ -3,14 +3,12 @@ package com.gdghackathon.monthlychallenges.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.gdghackathon.monthlychallenges.model.Challenge
 import com.gdghackathon.monthlychallenges.model.ChallengeRepository
-import kotlinx.coroutines.launch
 
-class ChallengeListViewModel : ViewModel() {
+class SampleChallengesViewModel : ViewModel() {
     private val repository = ChallengeRepository
 
-    private val _challengeList = MutableLiveData<List<Challenge>>()
-    val challengeList: LiveData<List<Challenge>> = _challengeList
+    private val _sampleChallenges = MutableLiveData<List<Challenge>>()
+    val sampleChallenges: LiveData<List<Challenge>> = _sampleChallenges
 }
