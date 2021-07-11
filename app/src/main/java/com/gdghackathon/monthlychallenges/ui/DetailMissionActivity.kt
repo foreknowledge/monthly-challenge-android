38149a78402be440943fafa8678ebf7e3b7ca5cf
Yang Cheol Joo <rlaengks0001@naver.com>
2021-07-11 09:45:55 +0900

    feat: DetailMissionActivity 화면 이동

---
+++ app/src/main/java/com/gdghackathon/monthlychallenges/ui/DetailMissionActivity.kt
@@ -0,0 +1,15 @@
+package com.gdghackathon.monthlychallenges.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gdghackathon.monthlychallenges.R
+
+class DetailMissionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_mission)
+
+
+    }
+}
