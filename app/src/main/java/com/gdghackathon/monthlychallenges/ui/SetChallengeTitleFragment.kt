b46b997fe58ed4d036a204303eb94bbe37a1cdd0
foreknowledge <kyj_0129@naver.com>
2021-07-12 22:21:30 +0900

    feat: 챌린지 이름 설정 화면 데이터 바인딩으로 전환

--- app/src/main/java/com/gdghackathon/monthlychallenges/ui/SetChallengeTitleFragment.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/ui/SetChallengeTitleFragment.kt
@@ -6,41 +6,36 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.widget.AppCompatButton
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.gdghackathon.monthlychallenges.R
+import com.gdghackathon.monthlychallenges.databinding.FragmentSetChallengeTitleBinding
 
 class SetChallengeTitleFragment : Fragment() {
     private var shortAnimationDuration: Int = 0
 
-    lateinit var challengeTitle: EditText
-    lateinit var challengeTitleDescription: TextView
+    private lateinit var binding: FragmentSetChallengeTitleBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_set_challenge_title, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_set_challenge_title, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
 
-        challengeTitle = view.findViewById(R.id.edittext_challenge_title)
-        challengeTitleDescription = view.findViewById(R.id.textview_challenge_title_description)
-        val confirmChallengeTitleButton =
-            view.findViewById<AppCompatButton>(R.id.button_confirm_challenge_title)
-
-        challengeTitle.addTextChangedListener(object : TextWatcher {
+        binding.edittextChallengeTitle.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (p0 != null) {
-                    confirmChallengeTitleButton.isEnabled = (p0.length in 1..20)
+                    binding.buttonConfirmChallengeTitle.isEnabled = (p0.length in 1..20)
 
                     if (p0.length > 20) {
-                        fadeIn(challengeTitleDescription)
+                        fadeIn(binding.textviewChallengeTitleDescription)
                     } else {
-                        challengeTitleDescription.visibility = View.GONE
+                        binding.textviewChallengeTitleDescription.visibility = View.GONE
                     }
                 }
             }
@@ -48,8 +43,6 @@ class SetChallengeTitleFragment : Fragment() {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
             override fun afterTextChanged(p0: Editable?) { }
         })
-
-        return view
     }
 
     private fun fadeIn(view: TextView) {
