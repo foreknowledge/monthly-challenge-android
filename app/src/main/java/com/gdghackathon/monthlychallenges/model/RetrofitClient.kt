158b826cf2556963e724bb6794614066c396155e
Yang Cheol Joo <rlaengks0001@naver.com>
2021-07-15 00:13:40 +0900

    fix: 응답 오류 해결

--- app/src/main/java/com/gdghackathon/monthlychallenges/model/RetrofitClient.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/model/RetrofitClient.kt
@@ -1,9 +1,12 @@
 package com.gdghackathon.monthlychallenges.model
 
 import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
 
 object RetrofitClient {
     private const val BASE_URL = "http://15.165.229.191:8080/"
@@ -21,7 +24,20 @@ object RetrofitClient {
         return instance ?: Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .client(client)
+                .addConverterFactory(NullOnEmptyConverterFactory())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
     }
 }
+
+class NullOnEmptyConverterFactory : Converter.Factory() {
+    override fun responseBodyConverter(type: Type?, annotations: Array<Annotation>?, retrofit: Retrofit?): Converter<ResponseBody, *>? {
+        val delegate = retrofit!!.nextResponseBodyConverter<Any>(this, type!!, annotations!!)
+        return Converter<ResponseBody, Any> {
+            if (it.contentLength() == 0L) return@Converter EmptyResponse()
+            delegate.convert(it)
+        }
+    }
+}
+
+class EmptyResponse
