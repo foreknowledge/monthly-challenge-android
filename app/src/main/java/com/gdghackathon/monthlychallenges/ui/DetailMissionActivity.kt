86e6f2c65028f609c6c6cd89045ef20fc50f24cb
foreknowledge <kyj_0129@naver.com>
2021-07-16 14:37:41 +0900

    feat: 미션 상세 화면 dataBinding 적용

--- app/src/main/java/com/gdghackathon/monthlychallenges/ui/DetailMissionActivity.kt
+++ app/src/main/java/com/gdghackathon/monthlychallenges/ui/DetailMissionActivity.kt
@@ -2,14 +2,15 @@ package com.gdghackathon.monthlychallenges.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.gdghackathon.monthlychallenges.R
+import com.gdghackathon.monthlychallenges.databinding.ActivityDetailMissionBinding
 
 class DetailMissionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailMissionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_mission)
-
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_mission)
     }
 }
